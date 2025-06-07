5ebe4b5c55f99da5048102883ab849794243a796
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-06-07 08:55:17 +0200

    Amazing main menu

--- Game/Objects/Terrain.gd
+++ Game/Objects/Terrain.gd
@@ -40,6 +40,7 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Enemy and effect == EffectType.SLOW:
+		print_debug("slowing ", body)
 		body.velocity *= .1
 	elif body is Enemy and effect == EffectType.KILL:
 		body.queue_free()
