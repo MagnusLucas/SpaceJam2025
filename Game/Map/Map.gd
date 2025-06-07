e3f2cd9b9e069ead131433dd5d71498c828e80d5
Zapzap <zuzannaa.pietrzak@gmail.com>
2025-06-07 14:06:05 +0200

    random word now exists
    hurray

--- Game/Map/Map.gd
+++ Game/Map/Map.gd
@@ -27,10 +27,10 @@ func _ready() -> void:
 	$"../../LoseOverlay".hide()
 	$"Player/Node2D/TextureProgressBar".show()
 	$"../../TextureProgressBar".value = 100
-	wave0()
+	#wave0()
 	while get_tree().get_node_count_in_group("Enemy") != 0:
 		await get_tree().create_timer(0.2).timeout
-	wave1()
+	wave2()
 	
 func update_terrain(prev_pos : Vector2i, curr_pos : Vector2i, terrain : Terrain) -> void:
 	var atlas_coords = get_cell_atlas_coords(prev_pos)
@@ -97,10 +97,22 @@ func wave_dead():
 	while get_tree().get_node_count_in_group("Enemy") != 0:
 		await get_tree().create_timer(0.2).timeout
 
+func random_word():
+	var word : String
+	#print($"../../Word") 
+	var a = $"../../Word".load_data()
+	var tab = []
+	for i in a:
+		tab.append(i)
+	word = tab.pick_random()
+	return word
+
 #----------------
 #ACTUALLY WAVE
 #----------------
 #wave0 to bedzie turorial
+
+
 func wave0():
 	add_child(Enemy.new_enemy('b',Vector2i(-250,-250),Vector2i(-250,-250),10,false))
 	while get_tree().get_node_count_in_group("Enemy") != 0:
@@ -124,7 +136,8 @@ func wave1():
 	
 
 func wave2():
-	var rand_word = 'placeholder' #tu kiedyß bedzie czytac z listy slow destruktywnych
+	random_word()
+	var rand_word = random_word() #tu kiedyß bedzie czytac z listy slow destruktywnych
 	var rand_let = string_to_array(rand_word)
 	first_drop(rand_let,Vector2i(50,-20),Vector2i(1100,-10),2)
 	for i in range(4):
@@ -134,7 +147,7 @@ func wave2():
 func wave6():
 	var rand_let : Array
 	for i in range(2):
-		var rand_word = 'placeholder' #tu kiedyß bedzie czytac z listy slow destruktywnych
+		var rand_word = random_word() #tu kiedyß bedzie czytac z listy slow destruktywnych
 		rand_let = rand_let + string_to_array(rand_word)
 	first_drop(rand_let,Vector2i(50,-20),Vector2i(1100,-10),2)
 	for i in range(4):
@@ -144,10 +157,10 @@ func wave6():
 func wave11():
 	var rand_let : Array
 	for i in range(3):
-		var rand_word = 'placeholder' #tu kiedyß bedzie czytac z listy slow destruktywnych
+		var rand_word = random_word() #tu kiedyß bedzie czytac z listy slow destruktywnych
 		rand_let = rand_let + string_to_array(rand_word)
 	while len(rand_let) < 10:
-		var rand_word = 'placeholder' #tu kiedyß bedzie czytac z listy slow destruktywnych
+		var rand_word = random_word() #tu kiedyß bedzie czytac z listy slow destruktywnych
 		rand_let = rand_let + string_to_array(rand_word)
 	first_drop(rand_let,Vector2i(50,-20),Vector2i(1100,-10),2)
 	#for i in range(4):
@@ -157,10 +170,10 @@ func wave11():
 func expo_wave(lvl : int, loc1 : Vector2i, loc2 : Vector2i):
 	var rand_let : Array
 	for i in range(lvl):
-		var rand_word = 'placeholder' #tu kiedyß bedzie czytac z listy slow destruktywnych
+		var rand_word = random_word() #tu kiedyß bedzie czytac z listy slow destruktywnych
 		rand_let = rand_let + string_to_array(rand_word)
 	while len(rand_let) < lvl*5:
-		var rand_word = 'placeholder' #tu kiedyß bedzie czytac z listy slow destruktywnych
+		var rand_word = random_word() #tu kiedyß bedzie czytac z listy slow destruktywnych
 		rand_let = rand_let + string_to_array(rand_word)
 	first_drop(rand_let,loc1,loc2,2)
 	for i in range(lvl*5):
